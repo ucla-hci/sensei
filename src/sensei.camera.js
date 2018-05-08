3e37205ad26e1cc08107538461786a6ed08f2711
hotnAny <anchen@anchen-macdesk.tsi.lan>
2018-05-08 15:36:57 -0700

    demonstrating the video feed from camera

--- src/sensei.camera.js
+++ src/sensei.camera.js
@@ -1,36 +1,52 @@
 //	........................................................................................................
 //
-//
+//  demonstrating the video feed from camera
 //
 //  by xiangchen@acm.org, v1.0 04/2018
 //
+//  ref: http://blog.teamtreehouse.com/accessing-the-device-camera-with-getusermedia
 //
 //	........................................................................................................
 
 var SENSEI = SENSEI || {};
 
 SENSEI.visualizations["camera"] = function() {
-  // Request the camera.
-  navigator.getUserMedia(
-    // Constraints
-    {
-      video: true
-    },
-    // Success Callback
-    function(localMediaStream) {
-      // Get a reference to the video element on the page.
-      var vid = document.getElementById("camera-stream");
+  if (navigator.getUserMedia) {
+    // for this demo, replace canvas tag with video tag
+    SENSEI.canvas = $("#canvasVis");
+    var positionInfo = SENSEI.canvas[0].getBoundingClientRect();
+    var width = positionInfo.width;
+    var height = positionInfo.height;
+    SENSEI.canvas.remove();
+    SENSEI.videoCamera = $("<video autoplay playsinline></video>");
+    SENSEI.videoCamera.css("max-width", width);
+    SENSEI.videoCamera.css("max-height", height);
+    $("#tdDemo").append(SENSEI.videoCamera);
 
-      // Create an object URL for the video stream and use this
-      // to set the video source.
-      vid.src = window.URL.createObjectURL(localMediaStream);
-    },
-    // Error Callback
-    function(err) {
-      // Log the error to the console.
-      console.log(
-        "The following error occurred when trying to use getUserMedia: " + err
-      );
-    }
-  );
+    // request the camera.
+    navigator.getUserMedia(
+      // constraints
+      {
+        video: true
+      },
+      // success Callback
+      function(localMediaStream) {
+        SENSEI.videoCamera.prop("srcObject", localMediaStream);
+      },
+      // error Callback
+      function(err) {
+        console.err(
+          "The following error occurred when trying to use getUserMedia: " + err
+        );
+      }
+    );
+  } else {
+    console.err("browser does not support getUserMedia");
+  }
+};
+
+SENSEI.clearings["camera"] = function() {
+  // restore the canvas
+  SENSEI.videoCamera.remove();
+  $("#tdDemo").append(SENSEI.canvas);
 };
